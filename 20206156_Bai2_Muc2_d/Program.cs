1c5e14d4787ee60b508c0bb6ee4a03ee4d32eabb
manh354 <104605422+manh354@users.noreply.github.com>
2022-05-05 14:33:31 +0700

    Add files via upload

---
+++ 20206156_Bai2_Muc2_d/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _20206156_Bai2_Muc2_y4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lan luot nhap vao 2 so a,b muon tim UCLN va BCNN: ");
+            int a, b;
+            Int32.TryParse(Console.ReadLine(), out a);
+            Int32.TryParse(Console.ReadLine(), out b);
+            int _a = a, _b = b;
+            int r;
+            while(b!=0)
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            }
+            Console.WriteLine("BCNN cua a va b la: {0}", a);
+            Console.WriteLine("UCLN cua a va b la: {0}", _a * _b / a);
+        }
+    }
+}
