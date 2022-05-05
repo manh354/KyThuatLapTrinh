1d18bf8faaa9c20fe0cb8ff3a19b32c8a5ffb711
manh354 <104605422+manh354@users.noreply.github.com>
2022-05-05 14:30:23 +0700

    Add files via upload

---
+++ 20206156_Bai1_Muc2_d/Program.cs
@@ -0,0 +1,32 @@
+﻿//Hoàng Đức Mạnh - 20206156
+
+using System;
+
+namespace _20206156_Bai1_Muc2_d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap vao chieu cao cua cay thong Noel");
+            int n;
+            while (!Int32.TryParse(Console.ReadLine(), out n))
+            {
+                Console.WriteLine("So da nhap vao khong hop le, nhap lai: ");
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n-i; j++)
+                    Console.Write(' ');
+                Console.Write('*');
+                for (int k = 0; k < 2*i;k++)
+                    Console.Write('*');
+                Console.WriteLine();
+            }
+            for (int j = 0; j < n; j++)
+                Console.Write(' ');
+            Console.Write('|');
+            Console.Read();
+        }
+    }
+}
