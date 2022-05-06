16d3cd395e9ad4882063eeb03334caecc1649132
manh354 <104605422+manh354@users.noreply.github.com>
2022-05-06 09:36:41 +0700

    Add files via upload

--- 20206156_Bai2_Muc2_b/Program.cs
+++ 20206156_Bai2_Muc2_b/Program.cs
@@ -8,26 +8,33 @@ namespace _20206156_Bai2_Muc2_b
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Nhap vao so nguyen can xet: ");
-            int n; bool flag = true;
-            while (!Int32.TryParse(Console.ReadLine(), out n))
+            while (true)
             {
-                Console.WriteLine("Du lieu nhap vao khong dung, nhap lai: ");
-            };
-            for(int i = 2; i<= Math.Sqrt(n); i++)
-            {
-                if (n % i == 0)
+                Console.WriteLine("Nhap vao so nguyen can xet: ");
+                int n; bool flag = true; string s;
+                s = Console.ReadLine();
+                if (s == "\\s") goto BREAK;
+                while (!Int32.TryParse(s, out n))
+                {
+                    Console.WriteLine("Du lieu nhap vao khong dung, nhap lai: ");
+                };
+                for (int i = 2; i <= Math.Sqrt(n); i++)
+                {
+                    if (n % i == 0)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (n == 0 || n == 1)
                 {
                     flag = false;
-                    break;
-                }                 
+                }
+                string output = flag ? "{0} la mot so nguyen to." : "{0} khong phai la mot so nguyen to.";
+                Console.WriteLine(output, n);
+                Console.ReadLine();
             }
-            if(n ==0 || n==1)
-            {
-                flag = false;
-            }    
-            string output = flag ? "{0} la mot so nguyen to." : "{0} khong phai la mot so nguyen to.";
-            Console.WriteLine(output, n);
+            BREAK: return;
         }
     }
 }
